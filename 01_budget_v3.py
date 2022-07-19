d11818949ae6cdaa588c38af25a1139a08cefba1
Ayesha Javed <101297266+ayeshajav@users.noreply.github.com>
2022-07-19 01:38:11 +0000

    Critiquing errors in the weight code

---
+++ 01_budget_v3.py
@@ -0,0 +1,20 @@
+#Functions go here
+
+#not blank
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "": 
+            return response 
+           
+        else:
+            print (error_message)
+
+
+#Main Routine goes here
+budget = not_blank("budget: ", "please enter the budget")
+
+
--- 01_weight_v3.py
+++ 01_weight_v3.py
@@ -5,56 +5,31 @@ def not_blank(question, error_message):
     valid = False
 
     while not valid:
-        response = int(input(question))
+        response = input(question)
 
-        if response != "": 
-            return response 
+        if response != "" and not response.isalpha(): 
+            return float(response)
         else:
             print (error_message)
 
 
+weight = not_blank("Weight: ", "Please enter the weight")
+  
+unit = input("(g)rams or (kg)Kilograms: ")
 
-
-def get_weight():
-  product_weight = {
-  "weight": 0,
- }
-  
-  weight = not_blank("Weight: ", "Please enter the weight")
-  
-  end_loop = False
-  
-  while not end_loop:
-    unit = input("(g)rams or (kg)Kilograms: ")
-    if unit.upper() == "G" or unit.upper() == "GRAMS":
+if unit.upper() == "G" or unit.upper() == "GRAMS":
       converted = weight / 1000
       print(f"You are {converted} kilos")
-      product_weight['weight'] = converted
-      end_loop = True
-    elif unit.upper() == "KG" or unit.upper() == "KILOGRAMS":
+     
+      
+elif unit.upper() == "KG" or unit.upper() == "KILOGRAMS":
       converted = weight 
       print(f"You are {converted} kilos")
-      product_weight['weight'] = converted
-      end_loop = True
-    else:
-      print("Please choose an appropriate choice")
-
-    return product_weight
-
-weight = get_weight()
-
-print(weight)
+      
+    
+else:
+    print("Please choose an appropriate choice")
 
 
-product = {
-  "name": "I am rice",
-}
-product.update (weight)
 
-print(product)
-
-loop 
-Productssss = [
-  {'name': 'I am rice', 'weight': 0.044}
-]
 
